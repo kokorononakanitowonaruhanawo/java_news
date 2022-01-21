056591321f5a0e9229e6d0246da673d0856d62b8
関戸基之 <sonegaukyokonogorodesu@gmail.com>
2022-01-21 15:20:17 +0900

    検索する関数を追加

--- src/main/java/dao/AdminDAO.java
+++ src/main/java/dao/AdminDAO.java
@@ -11,6 +11,10 @@ import database.DBConnection;
 import model.AdminModel;
 import util.settings.DBSettings;
 
+/**
+ * Admin DAO
+ */
+
 public class AdminDAO {
 
 	/**
@@ -62,7 +66,7 @@ public class AdminDAO {
 			DBConnection db = new DBConnection();
 			Connection connection = db.getConnection();
 			// SQL文を作成
-			String sql = "SELECT * FROM admin WHERE id=?";
+			String sql = "SELECT * FROM admin WHERE id = ?";
 			PreparedStatement stmt = connection.prepareStatement(sql);
 			// パラメーターを設定
 			stmt.setInt(1, id);
@@ -101,11 +105,49 @@ public class AdminDAO {
 			DBConnection db = new DBConnection();
 			Connection connection = db.getConnection();
 			// SQL文を作成
-			String sql = "SELECT * FROM admin WHERE email=? AND pass=?";
+			String sql = "SELECT * FROM admin WHERE email = ? AND pass = ?";
 			PreparedStatement stmt = connection.prepareStatement(sql);
 			// パラメーターを設定
 			stmt.setString(1, email);
 			stmt.setString(2, password);
+			// SQL文を実行
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				admin.setId(rs.getInt("id"));
+				admin.setEmail(rs.getString("email"));
+				admin.setName(rs.getString("name"));
+				admin.setPassword(rs.getString("pass"));
+			} else {
+				admin = null;
+			}
+		} catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return admin;
+	}
+	
+	
+	
+
+	/**
+	 * ****************************************************
+	 * 指定の管理者を取得
+	 * @param email
+	 * @return 検索結果（AdminModel）
+	 * ****************************************************
+	 */
+	public AdminModel findOne(String email) {
+		AdminModel admin = new AdminModel();
+		try {
+			// DBに接続
+			DBConnection db = new DBConnection();
+			Connection connection = db.getConnection();
+			// SQL文を作成
+			String sql = "SELECT * FROM admin WHERE email = ?";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			// パラメーターを設定
+			stmt.setString(1, email);
 			// SQL文を実行
 			ResultSet rs = stmt.executeQuery();
 			if(rs.next()) {
@@ -144,7 +186,12 @@ public class AdminDAO {
 							+ "pass, "
 							+ "name, "
 							+ "registration_date "
-							+ ") VALUES ( ?, ?, ?, ? )";
+							+ ") VALUES ( "
+							+ "?, "	// email
+							+ "?, "	// pass
+							+ "?, "	// name
+							+ "? "	// registration_date
+							+ ")";
 				
 			// SQLを実行する準備
 			PreparedStatement stmt = connection.prepareStatement(sql);
@@ -198,7 +245,11 @@ public class AdminDAO {
 			stmt.setString	(1, admin.getEmail());
 			stmt.setString	(2, admin.getPassword());
 			stmt.setString	(3, admin.getName());
-			stmt.setInt		(4, admin.getIsDeleted());
+			if(admin.getIsDeleted() == 0) {
+				stmt.setInt	(4, 0);
+			} else {
+				stmt.setInt	(4, admin.getIsDeleted());
+			}
 			stmt.setInt		(5, admin.getId());
 			
 			// SQLを実行
